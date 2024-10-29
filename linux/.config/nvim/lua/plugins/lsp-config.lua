dbf24bd0c51001f3a9e1e871fe2125456997b658
Daniel <daniel@thinkpad-t14.localdomain>
2024-10-29 17:15:23 +0100

    Add clangd LSP to nvim

--- linux/.config/nvim/lua/plugins/lsp-config.lua
+++ linux/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return{
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "jedi_language_server"}
+                ensure_installed = { "lua_ls", "jedi_language_server","clangd"}
             })
         end
     },
@@ -17,8 +17,9 @@ return{
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({}) -- lua LSP
             lspconfig.jedi_language_server.setup({}) -- Python LSP
+            lspconfig.clangd.setup({}) -- C LSP
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set({'n'},'<leader>ca', vim.lsp.buf.code_action, {})
         end
