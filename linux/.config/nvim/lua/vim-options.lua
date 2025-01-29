2720979cedc3e99330992c4262cc4e5b34d94b74
[Daniel Majewski] <[daniel.majewski04@gmail.com]>
2025-01-30 00:14:06 +0100

    Add auto-session and comment plugins

--- linux/.config/nvim/lua/vim-options.lua
+++ linux/.config/nvim/lua/vim-options.lua
@@ -14,7 +14,7 @@ vim.opt.number = true
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
-
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  [!] Requires https://github.com/equalsraf/win32yank to use nvim-wsl with windows clipboard
