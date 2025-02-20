a3814f6f69e8b8454a343a07e5b3d3d1ad46dbac
[Daniel Majewski] <[daniel.majewski04@gmail.com]>
2025-02-20 13:36:05 +0100

    Remap Open Vertical in Telescope to CTRL+Q

--- linux/.config/nvim/lua/plugins/telescope.lua
+++ linux/.config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,16 @@ return{
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
+
+            require("telescope").setup{
+                defaults = {
+                    mappings = {
+                        i = { ["<C-q>"] = require('telescope.actions').select_vertical },
+                        n = { ["<C-q>"] = require('telescope.actions').select_vertical },
+                    }
+                }
+            }
+
             vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
             vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
