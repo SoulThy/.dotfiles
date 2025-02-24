2239f543626bcea2cb1d99a0e899c6ac49afd0a9
[Daniel Majewski] <[daniel.majewski04@gmail.com]>
2025-02-24 20:13:08 +0100

    Improve and Remove treesitter config

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,33 @@
-return{
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = {"vimdoc","c", "python", "markdown", "lua", "vim", "query", "html"},
-			highlight = {enable = true},
-			indent = {enable = true},
-		})
-	end
+local ensure_installed = {
+    "vimdoc",
+    "c",
+    "python",
+    "markdown",
+    "lua",
+    "vim",
+    "query",
+    "html",
+    "diff",
+    "bash",
+}
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            ensure_installed = ensure_installed,
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+            modules = {},
+            sync_install = false,
+            ignore_install = {},
+        })
+    end
 }
